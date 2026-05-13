ddf05143c2a822470c135f207885c8eecc8763cf
s2wen <shuwenwen1118@gmail.com>
2026-05-12 21:10:26 -0700

    changed proxy back

--- my-app/proxy.ts
+++ my-app/proxy.ts
@@ -22,7 +22,7 @@ export const proxy = auth((req) => {
 })
 
 export const config = {
-  matcher: ["/home/:path*", "/onboarding", "/secret/:path*"]
+  matcher: ["/home/:path*", "/onboarding", "/secret/:path*", "/homepage/:path*", "/confirmation", "/review", "/profile/:path*"]
 
-  // , "/homepage/:path*", "/confirmation", "/review", "/profile/:path*"
+  
 }
